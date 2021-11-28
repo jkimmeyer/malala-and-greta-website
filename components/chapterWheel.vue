b1f58b1332a7ad2f12d68f12e1d8ea896b909cfe
cedricdsc <cedric.dsc@hotmail.de>
2021-11-28 12:51:53 +0100

    add chapterWheel component

---
+++ components/chapterWheel.vue
@@ -0,0 +1,86 @@
+<template>
+  
+    hi
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+@keyframes i {
+  from {transform: rotateZ(0deg);}
+  to {transform: rotateZ(70deg);}
+}
+
+.timeline-wrapper{
+  background-color: #936b3478;
+  width: 600px;
+  height: 600px;
+  border-radius: 50%;
+  position: relative;
+  transform: rotateX(70deg);
+
+  
+  .timestamp-spinner{
+    width: 100%;
+    position: absolute;
+    bottom: 50%;
+    transform: translate(0,calc(-50% + 45px));
+    transition: transform ease-in-out 1s;
+    margin: 0 0 -16px 0;
+    
+    &.i{
+      transform: rotateZ(-90deg);
+    }
+    
+    &.ii{
+      transform: rotateZ(-45deg);
+    }
+    
+    &.iii{
+      transform: rotateZ(0deg);
+    }
+    
+    &.iv{
+      transform: rotateZ(45deg);
+    }
+    &.v{
+      transform: rotateZ(90deg);
+    }
+    
+    
+    .timestamp{
+      font-size: 32px;
+      transform: rotateX(0deg) rotateY(0deg) rotateZ(90deg);
+      width: fit-content;
+      
+      
+      &:hover{
+        font-weight: bold;
+        cursor: pointer;
+      }
+    }
+  }
+  .description{
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%) rotateX(0deg);
+      font-size: 48px;
+    }
+}
+.btn-wrapper{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  
+  #spin-btn-prev, #spin-btn-next{
+    width: 100px;
+  }
+}
+
+
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  build: {
+    loaders: {
+      sass: {
+        implementation: require('sass'),
+      },
+      scss: {
+        implementation: require('sass'),
+      },
+    },
+  },
   css: [
     '@/assets/stylesheets/globals/colors.css',
     '@/assets/stylesheets/globals/font-sizes.css',
