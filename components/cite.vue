7f97e41441c34d3dc6a67535df5fe5511f956893
Fabian H <fab.h@gmx.de>
2021-12-11 15:05:58 +0100

    feat: add basic loop to cite component WIP

--- components/cite.vue
+++ components/cite.vue
@@ -1,18 +1,51 @@
 <template>
-  <p class="cite">
-    This is a quote \n
-    - Malala
-  </p>
+  <div class="cite" :style="position" @click="loop">
+    {{ citeText }}
+  </div>
 </template>
 
 <script>
+import content from '~/assets/cites.json'
+import gretaAudio from '~/assets/audio/cites/greta/greta-house-on-fire.mp3'
 export default {
-
+  data () {
+    return {
+      citeText: 'Das ist ein cite',
+      position: {
+        top: '10px',
+        left: '50px'
+      },
+      author: '',
+      index: 0
+    }
+  },
+  mounted () {
+    console.log('cite mounted')
+    // this.loop()
+  },
+  methods: {
+    loop () {
+      console.log('run loop')
+      this.index = (this.index + 1) % content.length
+      const nextItem = content[this.index]
+      this.citeText = nextItem.text
+      this.position.top = nextItem.top
+      this.position.left = nextItem.left
+      this.author = nextItem.author
+      const audio = new Audio(gretaAudio)
+      audio.play()
+      setTimeout(() => {
+        this.loop()
+      }, 5000)
+    }
+  }
 }
 </script>
 
-<style>
-  .cite {
-    position: absolute;
-  }
+<style scoped lang="scss">
+.cite {
+  position: absolute;
+  top: 50px;
+  left: 70px;
+}
 </style>
