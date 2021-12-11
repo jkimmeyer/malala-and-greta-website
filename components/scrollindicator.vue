9ae7cd051eee25f56e7fe12dd914d28a52bc08e1
Fabian H <fab.h@gmx.de>
2021-12-11 10:07:08 +0100

    chore: adapt code style

--- components/scrollindicator.vue
+++ components/scrollindicator.vue
@@ -2,31 +2,37 @@
   <div class="scroll-indicator">
     <div class="scroll-indicator--line dot" />
     <div class="scroll-indicator--line" />
-    <p class="scroll-indicator--message">scroll to continue</p>
+    <p class="scroll-indicator--message">
+      scroll to continue
+    </p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style scoped>
 .scroll-indicator {
   display: flex;
+  /* stylelint-disable-next-line declaration-no-important */
   transform: translateX(-50%) rotate(-90deg) !important;
 }
+
 .scroll-indicator--message {
   align-self: center;
 }
 
 .scroll-indicator--line {
+  display: inline-block;
+  align-self: center;
   width: 80px;
   height: 3px;
   margin-right: 15px;
-  align-self: center;
+  /* stylelint-disable-next-line color-named */
   background-color: black;
-  display: inline-block;
 }
+
 .dot {
   width: 3px;
   margin-right: 5px;
