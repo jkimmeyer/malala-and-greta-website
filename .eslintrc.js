d0f739d546b059c7428775f27d2bc3c4afa9f633
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2021-12-15 09:22:37 +0100

    Adapt troisjs component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: [
     '@nuxtjs/eslint-config-typescript'
-  ]
+  ],
+  env: {
+    browser: true,
+    node: true
+  }
 }
--- components/troisjs.vue
+++ components/troisjs.vue
@@ -1,23 +1,28 @@
 <template>
-  <Renderer ref="renderer">
-    <Camera :position="{ z: 10 }" />
-    <Scene>
-      <PointLight :position="{ y: 50, z: 50 }" />
-      <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
-        <LambertMaterial />
-      </Box>
-    </Scene>
-  </Renderer>
+  <client-only>
+    <Renderer>
+      <Camera :position="{ z: 10 }" />
+      <Scene>
+        <PointLight :position="{ y: 50, z: 50 }" />
+        <Box>
+          <LambertMaterial />
+        </Box>
+      </Scene>
+    </Renderer>
+  </client-only>
 </template>
 
 <script>
+import { Box, Camera, LambertMaterial, PointLight, Renderer, Scene } from 'troisjs'
+
 export default {
-  mounted() {
-    const renderer = this.$refs.renderer;
-    const box = this.$refs.box.mesh;
-    renderer.onBeforeRender(() => {
-      box.rotation.x += 0.01;
-    });
-  },
-};
+  components: {
+    Box,
+    Camera,
+    LambertMaterial,
+    PointLight,
+    Renderer,
+    Scene
+  }
+}
 </script>
