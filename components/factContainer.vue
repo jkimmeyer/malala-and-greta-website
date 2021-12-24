0478eabf286eb1be8066325d896ed40e2837374c
cedricdsc <cedric.dsc@hotmail.de>
2021-12-24 13:07:57 +0100

    add styling and bg-cards to factContainer

--- components/factContainer.vue
+++ components/factContainer.vue
@@ -1,20 +1,26 @@
 <template>
 
   <div :class="'fact-container '+postData.size+' '+postData.theme">
-    <div class="prev-arrow arrow" @click="previousFact()"> 	&lt; </div>
-    <!-- eslint-disable vue/require-v-for-key -->
-    <div v-for="(fact, index) in facts">
-      <div :class="'fact fact-'+index" v-if="activeFactID === index">
-        <h3>{{fact.headline}}</h3>
-        <!-- eslint-disable vue/require-v-for-key -->
-        <p v-for="paragraph in fact.paragraphs">
-          {{paragraph}}
-        </p>
-        <!-- eslint-enable -->
-        <p>Quelle: {{fact.source}}</p>
+    <div class="fact-bg-card"></div>
+    <div class="fact-bg-card"></div>
+    <div class="fact-card">
+      <div class="prev-arrow arrow" @click="previousFact()"> 	&lt; </div>
+      <!-- eslint-disable vue/require-v-for-key -->
+      <div class="facts-wrapper" v-for="(fact, index) in facts">
+        <div :class="'fact fact-'+index" v-if="activeFactID === index">
+          <h3>{{fact.headline}}</h3>
+          <!-- eslint-disable vue/require-v-for-key -->
+          <div class="paragraph-wrapper">
+            <p v-for="paragraph in fact.paragraphs">
+              {{paragraph}}
+            </p>
+          </div>
+          <!-- eslint-enable -->
+          <p>Quelle: {{fact.source}}</p>
+        </div>
       </div>
+      <div class="next-arrow arrow" @click="nextFact()"> 	&gt; </div>
     </div>
-    <div class="next-arrow arrow" @click="nextFact()"> 	&gt; </div>
   </div>
 
 </template>
@@ -50,15 +56,69 @@ export default{
 
 <style lang="scss" scoped>
 .fact-container{
+  //start: only for dev purpose
+  margin:5rem 0 0 5rem;
+  //end: only for dev purpose
+
   min-width: 256px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  border-radius: 10px;
+  position: relative;
+
+  .fact-card, .fact-bg-card{
+    box-shadow: 0 8px 8px rgba(0,0,0,0.25);
+  }
+
+  .fact-card{
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    z-index: 2;
+
+    .facts-wrapper{
+      height: 100%;
+
+      .fact{
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+
+        h3{
+          font-family: Trochut;
+          font-weight: bold;
+        }
+
+        p{
+          font-family: Lato;
+          font-style: italic;
+        }
+
+        .paragraph-wrapper p{
+          margin: 0.5rem 0;
+        }
+      }
+    }
+  }
+
+  .fact-bg-card{
+    position: absolute;
+
+    &:nth-child(1){
+      top: 25px;
+      left: -15px;
+      z-index: 0
+    }
+
+    &:nth-child(2){
+      transform: rotate(4deg);
+      top: 10px;
+      left: 10px;
+      z-index: 1;
+    }
+  }
 
 
   .arrow{
-    height: 100%;
     padding: 1rem;
     cursor: pointer;
 
@@ -68,25 +128,39 @@ export default{
   }
 
   &.sm{
-    max-width: 256px;
+    .fact-card, .fact-bg-card{
+      width: 256px;
+      max-width: 256px;
+      height: 256px;
+      min-height: 256px;
+    }
   }
 
   &.lg{
-    max-width: 512px;
+    .fact-card, .fact-bg-card{
+      width: 512px;
+      max-width: 512px;
+      height: 512px;
+      min-height: 512px;
+    }
   }
 
   &.greta{
-    background-color: #92BAE4;
+    .fact-card, .fact-bg-card{
+      background-color: #92BAE4;
 
-    & h3{
-      color: #445569;
+      & h3{
+        color: #445569;
+      }
     }
   }
   &.malala{
-    background-color: #E1BC84;
+    .fact-card, .fact-bg-card{
+      background-color: #E1BC84;
 
-    & h3 {
-      color: #68573D;
+      & h3 {
+        color: #68573D;
+      }
     }
   }
 }
