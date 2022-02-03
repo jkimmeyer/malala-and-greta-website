4e4cee96d35c1d818383d439e9d5c53b880ce859
Fabian H <fab.h@gmx.de>
2022-02-03 21:30:19 +0100

    feat: Add smooth scrolling

--- composables/useAnimation.ts
+++ composables/useAnimation.ts
@@ -2,29 +2,15 @@ import { gsap } from 'gsap/dist/gsap.js'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.js'
 
 export const useAnimation = () => {
-  const groupName = 'group-all'
+  const groupPage = 'group-page'
+  const groupChapter = 'group-chapter'
   const showMarkers = false
 
+  gsap.registerPlugin(ScrollTrigger)
+
+  // group handling
   // const getAllOfGroup = group => ScrollTrigger.getAll().filter(t => t.vars.group === group)
-  // const start = () => {
-  //   console.log(getAllOfGroup(groupName))
-  //   gsap.registerPlugin(ScrollTrigger)
-  //   getAllOfGroup(groupName).forEach(t => t.enable())
-  // }
-  // const stop = () => {
-  //   getAllOfGroup(groupName).forEach(t => t.kill())
-  // }
-  // const parseValue = (element) => {
-  //   let animationValue
-  //   if (element.classList.contains()) {
-  //     element.classList.forEach((c) => {
-  //       if (c.startsWith('animation-value-')) {
-  //         animationValue = c.substring(0, 16)
-  //       }
-  //     })
-  //     return animationValue
-  //   }
-  // }
+  // getAllOfGroup(groupName).forEach(t => t.enable())
 
   const pin = (element) => {
     return gsap.to(element, {
@@ -32,19 +18,7 @@ export const useAnimation = () => {
         trigger: element,
         pin: true,
         pinSpacing: false,
-        group: groupName
-      }
-    })
-  }
-
-  const animateBackgroundColor = (element, backgroundColor) => {
-    return gsap.to('.page', {
-      backgroundColor,
-      scrollTrigger: {
-        trigger: element,
-        start: 'top center',
-        scrub: true,
-        group: groupName
+        group: groupChapter
       }
     })
   }
@@ -56,7 +30,7 @@ export const useAnimation = () => {
       scrollTrigger: {
         trigger: element,
         scrub: true,
-        group: groupName
+        group: groupChapter
       }
     })
   }
@@ -78,7 +52,7 @@ export const useAnimation = () => {
         trigger: element,
         toggleActions: 'restart none none reset',
         markers: showMarkers,
-        group: groupName
+        group: groupChapter
       }
     })
   }
@@ -88,7 +62,6 @@ export const useAnimation = () => {
   }
 
   const animateAll = () => {
-    gsap.registerPlugin(ScrollTrigger)
     gsap.utils.toArray('.reveal-left').forEach((element) => {
       hide(element)
       reveal(element, -100)
@@ -108,8 +81,95 @@ export const useAnimation = () => {
     })
   }
 
+  const animatePageBackgroundColor = (element, backgroundColor) => {
+    return gsap.to('.page', {
+      backgroundColor,
+      scrollTrigger: {
+        trigger: element,
+        start: 'top center',
+        scrub: true,
+        group: groupPage
+      }
+    })
+  }
+
+  // inspired by:
+  // https://codepen.io/GreenSock/pen/gOgWELo
+  // https://greensock.com/docs/v3/Plugins/ScrollTrigger/static.scrollerProxy()
+  const applySmoothScrollToPage = (window, content, viewport) => {
+    const smoothness = 0.8
+    content = gsap.utils.toArray(content)[0]
+    viewport = gsap.utils.toArray(viewport)[0]
+    gsap.set(viewport, { overflow: 'hidden', position: 'fixed', height: '100%', width: '100%', top: 0, left: 0, right: 0, bottom: 0 })
+    gsap.set(content, { overflow: 'visible', width: '100%' })
+
+    const getProp = gsap.getProperty(content)
+    const setProp = gsap.quickSetter(content, 'y', 'px')
+    const setScroll = ScrollTrigger.getScrollFunc(window)
+    const removeScroll = () => {
+      content.style.overflow = 'visible'
+    }
+    const killScrub = (trigger) => {
+      const scrub = trigger.getTween ? trigger.getTween() : gsap.getTweensOf(trigger.animation)[0]
+      scrub && scrub.kill()
+      trigger.animation.progress(trigger.progress)
+    }
+    let height; let isProxyScrolling
+
+    function refreshHeight () {
+      height = content.clientHeight
+      content.style.overflow = 'visible'
+      document.body.style.height = height + 'px'
+      return height - document.documentElement.clientHeight
+    }
+
+    ScrollTrigger.addEventListener('refresh', () => {
+      removeScroll()
+      requestAnimationFrame(removeScroll)
+    })
+    ScrollTrigger.defaults({ scroller: content })
+
+    ScrollTrigger.scrollerProxy(content, {
+      scrollTop (value) {
+        if (arguments.length) {
+          isProxyScrolling = true // otherwise, if snapping was applied (or anything that attempted to SET the scroll proxy's scroll position), we'd set the scroll here which would then (on the next tick) update the content tween/ScrollTrigger which would try to smoothly animate to that new value, thus the scrub tween would impede the progress. So we use this flag to respond accordingly in the ScrollTrigger's onUpdate and effectively force the scrub to its end immediately.
+          setProp(-value)
+          setScroll(value)
+          return
+        }
+        return -getProp('y')
+      },
+      scrollHeight: () => document.body.scrollHeight,
+      getBoundingClientRect () {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight }
+      }
+    })
+
+    return ScrollTrigger.create({
+      animation: gsap.fromTo(content, { y: 0 }, {
+        y: () => document.documentElement.clientHeight - height,
+        ease: 'none',
+        onUpdate: ScrollTrigger.update
+      }),
+      scroller: window,
+      invalidateOnRefresh: true,
+      start: 0,
+      end: refreshHeight,
+      refreshPriority: -999,
+      scrub: smoothness,
+      onUpdate: (self) => {
+        if (isProxyScrolling) {
+          killScrub(self)
+          isProxyScrolling = false
+        }
+      },
+      onRefresh: killScrub // when the screen resizes, we just want the animation to immediately go to the appropriate spot rather than animating there, so basically kill the scrub.
+    })
+  }
+
   return {
     animateAll,
-    animateBackgroundColor
+    applySmoothScrollToPage,
+    animatePageBackgroundColor
   }
 }
