e6712ffe058ddc88792d52aa8c3e2a1eaa36bd00
Fabian H <fab.h@gmx.de>
2022-03-09 00:47:02 +0100

    feat: add end WIP

--- components/End.vue
+++ components/End.vue
@@ -3,27 +3,98 @@
     <div class="intro">
       End
     </div>
-    <div class="content">
+    <div>
       <div class="mx-40 2xl:m-auto max-w-screen-xl">
         <div class="grid grid-cols-12">
-          <div class="col-start-3 col-span-9" data-animate-parallax="100">
+          <div class="col-start-4 col-span-6">
             <Image
               :decoration-top-left="true"
               :decoration-top-right="true"
               :decoration-bottom-left="true"
               :decoration-bottom-right="true"
               theme="default"
-              path="/images/chapter/4/malala/malala-kapt4-bus.jpeg"
+              path="/images/end/both-sitting-bench.jpeg"
             />
           </div>
         </div>
 
-        <div class="grid grid-cols-12">
-          <div class="text text-right col-start-2 col-span-4" data-narrator-malala="bullet-silence" data-animate-reveal-right>
+        <div class="grid grid-cols-12 mt-20">
+          <div class="body2 greta col-start-3 col-span-3 " data-narrator-malala="bullet-silence" data-animate-reveal-left>
             “So today I met my role model. What else can I say?”
           </div>
-          <div class="text text-right col-start-6 col-span-12" data-narrator-malala="bullet-silence" data-animate-reveal-right>
+          <div class="body2 malala col-start-8 col-span-3 mt-20" data-narrator-malala="bullet-silence" data-animate-reveal-right>
             “She's the only friend I'd skip school for”
+          </div>
+        </div>
+
+        <div class="grid grid-cols-12 mt-60 items-center">
+          <div class="col-start-2 col-span-3">
+            <a href="https://fridaysforfuture.de/">
+              <Image
+                class="mr-5"
+                theme="default"
+                path="/images/end/logo-friday-for-future.png"
+              />
+            </a>
+          </div>
+          <div class="col-start-5 col-span-5" data-animate-reveal-right>
+            <a class="link-greta" href="https://fridaysforfuture.de/">Fridays for Future</a><br>
+            <span class="body2 greta">Das sind alle, die für unser Klima auf die Straße gehen.</span>
+          </div>
+        </div>
+        <div class="grid grid-cols-12 my-20 items-center">
+          <div class="text-right col-start-4 col-span-4" data-animate-reveal-left>
+            <a class="link-malala" href="https://malala.org/">Malala Fund</a><br>
+            <span class="body2 malala">Working for a world where every girl can learn and lead.</span>
+          </div>
+          <div class="col-start-8 col-span-3">
+            <a href="https://malala.org/">
+              <Image
+                class="ml-5"
+                theme="default"
+                path="/images/end/logo-malala-fund.png"
+              />
+            </a>
+          </div>
+        </div>
+        <div class="grid grid-cols-12 mt-60">
+          <div class="body2 text-center col-start-3 col-span-8">
+            Wir erzählen die Geschichten von zwei starken jungen Frauen, die nie aufgehört haben für Gerechtigkeit, Wahrheit und Gleichheit zu kämpfen. Sie sind Vorbilder, die uns vor Augen führen, dass jeder etwas verändern und in der Welt bewirken kann.
+          </div>
+        </div>
+        <!--- world and rocket -->
+        <div class="grid grid-cols-12 mt-60">
+          <div class="col-start-6 col-span-1" data-animate-reveal-right>
+            <Image
+              class="mr-5"
+              theme="default"
+              path="/images/end/rocket.png"
+            />
+          </div>
+        </div>
+        <div class="grid grid-cols-12 items-center">
+          <div class="col-start-3 col-span-3">
+            <Image
+              class="mr-5"
+              theme="default"
+              path="/images/end/world.png"
+            />
+          </div>
+          <div class="body1 col-start-7 col-span-5" data-animate-reveal-right>
+            Was willst du verändern?
+          </div>
+        </div>
+        <!--- credits -->
+        <div class="grid grid-cols-12 mt-80">
+          <div class="body3 text-center col-start-2 col-span-10" data-animate-parallax="1">
+            <span class="highlight">Ein Projekt der</span> Hochschule Düsseldorf <br>
+            <span class="highlight">im Kurs</span> Digital Storytelling<br>
+            <span class="highlight">betreut von</span> Lukas Loss<br>
+          </div>
+        </div>
+        <div class="grid grid-cols-12 mt-50">
+          <div class="body3 text-center col-start-2 col-span-10">
+            <span class="highlight">erstellt von</span>  Cedrica Da Silva Costa, Joel Garthe, Fabian Heck, Johannes Kimmeyer, Nathalie Merdan, Sam Vogelskamp
           </div>
         </div>
       </div>
@@ -31,9 +102,90 @@
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .end {
   background-color: var(--bone);
+}
+
+.link-malala {
+  font-family: var(--sans-serif-font);
+  font-size: var(--font-size-body-1);
+  transition: color 0.2s ease-in-out;
+
+  &:link {
+    color: var(--color-text-malala-dark);
+    text-decoration: none;
+  }
+
+  &:visited {
+    color: var(--color-text-malala-dark);
+    text-decoration: none;
+  }
+
+  &:hover {
+    color: var(--white);
+    text-decoration: none;
+  }
+
+  &:active {
+    color: var(--color-text-malala-dark);
+    text-decoration: none;
+  }
+}
+.link-greta {
+  font-family: var(--sans-serif-font);
+  font-size: var(--font-size-body-1);
+  transition: color 0.2s ease-in-out;
+
+  &:link {
+    color: var(--color-text-greta-dark);
+    text-decoration: none;
+  }
+
+  &:visited {
+    color: var(--color-text-greta-dark);
+    text-decoration: none;
+  }
+
+  &:hover {
+    color: var(--white);
+    text-decoration: none;
+  }
+
+  &:active {
+    color: var(--color-text-greta-dark);
+    text-decoration: none;
+  }
+}
+
+.body1 {
+  font-family: var(--sans-serif-font);
+  font-size: var(--font-size-body-1);
+  color: var( --deeptaupe);
+}
+
+.body2 {
+  font-family: var(--sans-serif-font);
+  font-size: var(--font-size-body-2);
+  font-style: italic;
+  color: var(--deeptaupe);
+}
+
+.body3 {
+  font-family: var(--sans-serif-font);
+  font-size: var(--font-32);
+  color: var(--deeptaupe);
+}
+.highlight {
+  color: var(--white);
+}
+
+.malala {
+  color: var(--color-text-malala-dark);
+}
+
+.greta {
+  color: var(--color-text-greta-dark);
 }
 
 .intro {
@@ -45,11 +197,4 @@
   background: linear-gradient(var(--khaki) 0%, var(--bone) 100%);
 }
 
-.content {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-}
 </style>
