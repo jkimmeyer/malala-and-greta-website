bbf9b7930d8055a74d8f37057409e51ef56508f4
Fabian H <fab.h@gmx.de>
2022-03-11 11:03:53 +0100

    feat: add icons for intro

--- components/chapter/Five.vue
+++ components/chapter/Five.vue
@@ -6,7 +6,14 @@
           id="chapter-5"
           :chapter="5"
           :is-malala="true"
-        />
+        >
+          <div class="inline-flex flex-col ml-auto">
+            <h2 data-animate-reveal-left class="intro-heading">
+              Welche Reise hat mein Leben verändert?
+              <img class="inline-block" :src="require('~/assets/images/chapter/5/malala/malala-compass.png')" width="120">
+            </h2>
+          </div>
+        </ChapterIntro>
         <ChapterMalalaFive />
       </template>
 
@@ -15,7 +22,14 @@
           id="chapter-5"
           :chapter="5"
           :is-malala="false"
-        />
+        >
+          <div class="inline-flex flex-col ml-auto">
+            <h2 data-animate-reveal-left class="intro-heading">
+              Welche Reise hat mein Leben verändert?
+              <img class="inline-block" :src="require('~/assets/images/chapter/5/greta/greta-compass.png')" width="120">
+            </h2>
+          </div>
+        </ChapterIntro>
         <ChapterGretaFive />
       </template>
     </ChapterSwitcher>
