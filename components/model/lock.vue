f39d752d9711131642b42f5c598d32f86544d884
cedricdsc <cedric.dsc@hotmail.de>
2022-03-12 21:10:17 +0100

    add model components for chapter 2

---
+++ components/model/lock.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'Lock',
+  props: {
+    mesh: {
+      type: Object,
+      default: null
+    }
+  },
+  methods: {
+    animate (mesh) {
+      mesh.rotation.y = 0
+      mesh.rotation.x = 0
+      mesh.rotation.z = 0
+    },
+    getMeshPath () {
+      return '/models/door_and_lock.obj'
+    },
+    setInitialValues (mesh) {
+      mesh.scale.set(1, 1, 1)
+    }
+  }
+}
+</script>
