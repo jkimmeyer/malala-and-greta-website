281332256aa8fdbde392767c2b1f832129f17a3e
Fabian H <fab.h@gmx.de>
2022-03-12 13:00:50 +0100

    fix: remove unused imports

--- composables/audioSound.ts
+++ composables/audioSound.ts
@@ -1,7 +1,7 @@
 import { watch } from '@nuxtjs/composition-api'
 import { getCurrentTheme } from '@/composables/theme'
 import { Themes } from '@/enums/Themes'
-import { getAudioOn, setAudioOn } from '@/composables/audioMute'
+import { getAudioOn } from '@/composables/audioMute'
 
 // current audio sources
 let soundAudioSourceMalala: string = null
